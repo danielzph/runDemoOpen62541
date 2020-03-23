05a29ba3de5b910e3d04092c5e508c8bba18d784
danielzph <Daniel960915@163.com>
2020-03-23 20:58:12 +0800

    push test2 and 3

---
+++ cmake-build-debug/CMakeFiles/client_test2.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/client_test2.dir/src/client_test2.c.o"
+  "../bin/client_test2.pdb"
+  "../bin/client_test2"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/client_test2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/client_test3.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/client_test3.dir/src/client_test3.c.o"
+  "../bin/client_test3.pdb"
+  "../bin/client_test3"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/client_test3.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
